8a42f435cd3ad11d7e1e8dd4a55593fa66046652
sethu <sethu@DESKTOP-24CTG5U>
2021-09-09 08:02:04 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// C# program program to
+// check for even or odd
+using System;
+
+class GFG
+{
+    // Returns true if n is even, else odd
+    public static bool isEven(int n)
+    {
+        return (n % 2 == 0);
+    }
+
+    // Driver code
+    public static void Main()
+    {
+        int n = 102;
+        if (isEven(n) == true)
+            Console.WriteLine("Even");
+        else
+            Console.WriteLine("Odd");
+    }
+}
